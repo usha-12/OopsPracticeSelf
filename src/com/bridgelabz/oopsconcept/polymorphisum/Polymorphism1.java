abb23e0e8331f9f78af8cc27cefe2b90e2361f14
usha-12 <usha12ahirwar@gmail.com>
2022-10-26 00:33:42 +0530

    Polymorphism

--- src/com/bridgelabz/oopsconcept/polymorphisum/Polymorphism1.java
+++ src/com/bridgelabz/oopsconcept/polymorphisum/Polymorphism1.java
@@ -1,2 +1,40 @@
-package com.bridgelabz.oopsconcept.polymorphisum;public class Polymorphism1 {
+package com.bridgelabz.oopsconcept.polymorphisum;
+
+class Vehicle{
+    int x = 10;
+    void m1(){
+        System.out.println("method m1 ");
+    }
+}
+class Car extends Vehicle{
+    int y =20;
+    void m2(){
+        System.out.println("method m2");
+    }
+}
+class Test{
+    void methodA(Car car){
+        System.out.println("methodA (car)");
+    }
+    void methodB(int x){
+        System.out.println("methodB ");
+    }
+    void methodC(Vehicle vehicle){
+        System.out.println("methodC (vehicle)");
+    }
+}
+public class Polymorphism1 {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.methodB(100);
+        Car car = new Car();
+        test.methodA(car);
+        Vehicle vehicle = new Vehicle();
+        test.methodC(vehicle);
+        Vehicle ref = new Car();
+        test.methodA(car);
+        test.methodC(vehicle);
+        test.methodC(ref);
+
+    }
 }
