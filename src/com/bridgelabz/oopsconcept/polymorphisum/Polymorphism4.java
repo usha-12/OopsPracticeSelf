f6742ab9cb8401f4fdb7f6e2889df0501f68e5eb
usha-12 <usha12ahirwar@gmail.com>
2022-10-28 23:29:07 +0530

    Overriding return type should be same

--- src/com/bridgelabz/oopsconcept/polymorphisum/Polymorphism4.java
+++ src/com/bridgelabz/oopsconcept/polymorphisum/Polymorphism4.java
@@ -1,2 +1,26 @@
-package com.bridgelabz.oopsconcept.polymorphisum;public class Polymorphism4 {
+package com.bridgelabz.oopsconcept.polymorphisum;
+// while overriding the return types should be same or covariant(parent - child relationship)
+class AB{
+    Vehicle m1(){
+        System.out.println("Class AB m1 ");
+        return new Vehicle();
+    }
+
+}
+class ChildAB extends AB{
+    @Override
+    Car m1(){
+        System.out.println("class ChildAB m1");
+       // super.m1();
+        return new Car();
+
+    }
+
+}
+public class Polymorphism4 {
+    public static void main(String[] args) {
+        AB obj = new ChildAB();
+        obj.m1();
+
+    }
 }
