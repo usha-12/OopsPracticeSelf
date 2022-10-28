3eda63d5aed2248b63d0aba4c281ebf92a0b214a
usha-12 <usha12ahirwar@gmail.com>
2022-10-28 23:27:26 +0530

    Final KeyWord Example

--- src/com/bridgelabz/oopsconcept/finalKeyword/FinalKeyWord.java
+++ src/com/bridgelabz/oopsconcept/finalKeyword/FinalKeyWord.java
@@ -1,2 +1,29 @@
-package com.bridgelabz.oopsconcept.finalKeyword;public class FinalKeyWord {
+package com.bridgelabz.oopsconcept.finalKeyword;
+// final class Parent1{ - class can not be inherited
+
+
+    // method can not be overridden
+  /* final void m1(){
+        System.out.println("method parent");
+    }
+
+   */
+
+//}
+//class Child1 extends Parent1{
+   // @Override
+   // void m1(){
+       // System.out.println("method child ");
+    //}
+
+//}
+public class FinalKeyWord {
+    final int x = 100;
+    static final int y =200;
+
+    public static void main(String[] args) {
+        FinalKeyWord obj = new FinalKeyWord();
+        System.out.println(obj.x);
+        System.out.println(obj.y);
+    }
 }
